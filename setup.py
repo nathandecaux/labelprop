4fa4ffd8525968cde17fa91ff5987b39b0c15b8e
Nathan Decaux <nathan.decaux@hotmail.fr>
2024-02-12 10:49:41 +0100

    Updating version number

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/nathandecaux/labelprop',
-    version='1.2.1',
+    version='1.2.1.1',
     zip_safe=False,
 )
